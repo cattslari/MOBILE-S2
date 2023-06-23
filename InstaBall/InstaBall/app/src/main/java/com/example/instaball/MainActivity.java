69c16a0e0417fb587b7435b5077d805465b6511f
brunogcsousa <bruno.g.c.sousa@gmail.com>
2023-06-22 21:41:21 -0300

    Ultimo projeto android, instaball

---
+++ InstaBall/InstaBall/app/src/main/java/com/example/instaball/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.instaball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.projetoagenda.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btcadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        btcadastro = findViewById(R.id.cadastro);
+
+        btcadastro.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                segundaTela();
+            }
+        } );
+
+    }
+
+    private void segundaTela(){
+        Intent it = new Intent( this, Cadastro.class);
+        startActivityForResult( it, 1 );
+    }
+}
